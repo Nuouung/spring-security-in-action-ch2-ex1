c5a1fe30bc26b912824b5e05d4f1aac9bf45d5b8
Nuouung <jin6016@gmail.com>
2022-07-03 18:59:18 +0900

    https openssl 적용

---
+++ src/main/java/security/springsecurityinactionch2ex1/SpringSecurityInActionCh2Ex1Application.java
@@ -0,0 +1,13 @@
+package security.springsecurityinactionch2ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInActionCh2Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInActionCh2Ex1Application.class, args);
+	}
+
+}
