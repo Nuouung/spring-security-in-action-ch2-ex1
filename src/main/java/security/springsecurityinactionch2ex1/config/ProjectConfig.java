a7debdaea82cb32c98339292975530bb1c63c9f5
Nuouung <jin6016@gmail.com>
2022-07-07 19:12:37 +0900

    유연한 권한 설정이 가능한 스프링 시큐리티

--- src/main/java/security/springsecurityinactionch2ex1/config/ProjectConfig.java
+++ src/main/java/security/springsecurityinactionch2ex1/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package security.springsecurityinactionch2ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +12,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic();
+
+        http.authorizeRequests()
+                .anyRequest().authenticated(); // 모든 사용자에게 인증을 요구하다. <- 기본(default) 인증 설정과 같은 방식
+
+//        http.authorizeRequests()
+//                .anyRequest().permitAll(); // 모든 사용자에게 인증을 요구하지 않는다.
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
