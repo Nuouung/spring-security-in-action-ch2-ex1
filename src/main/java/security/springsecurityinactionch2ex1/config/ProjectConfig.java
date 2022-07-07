4bd018d33385160ff1cdb1564c08de27737e5ee0
Nuouung <jin6016@gmail.com>
2022-07-07 19:21:01 +0900

    WebSecurityConfigurerAdapter를 이용한 userDetailsService, passwordEncoder 등록

--- src/main/java/security/springsecurityinactionch2ex1/config/ProjectConfig.java
+++ src/main/java/security/springsecurityinactionch2ex1/config/ProjectConfig.java
@@ -1,14 +1,12 @@
 package security.springsecurityinactionch2ex1.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -21,30 +19,25 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .anyRequest().authenticated(); // 모든 사용자에게 인증을 요구하다. <- 기본(default) 인증 설정과 같은 방식
-
 //        http.authorizeRequests()
 //                .anyRequest().permitAll(); // 모든 사용자에게 인증을 요구하지 않는다.
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        // InMemoryUserDetailManager를 스프링 빈으로 등록한다. (기본적으로 장착되어 있는 UserDetailService 대체)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 빈으로 등록한 UserDetailsService와 PasswordEncoder를 WebSecurityConfigurerAdapter.configure를 통해 등록한다.
+        // 스프링 시큐리티의 권한 설정이 매우 유연함을 보여주는 예
         InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
 
-        // username과 비밀번호, 권한을 가진 임의의 회원을 생성한다.
         UserDetails user = User.withUsername("test")
                 .password("1234")
                 .authorities("read")
                 .build();
 
-        // InMemoryUserDetailManager가 생성한 회원을 관리하도록 한다.
         userDetailsService.createUser(user);
 
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
