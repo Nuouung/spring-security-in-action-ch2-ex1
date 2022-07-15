abd2c6ab7b52a542c3176a862193f90c3c64ec01
Nuouung <jin6016@gmail.com>
2022-07-16 02:45:15 +0900

    commit

--- src/main/java/security/springsecurityinactionch2ex1/config/WebAuthorizationConfig.java
+++ src/main/java/security/springsecurityinactionch2ex1/config/WebAuthorizationConfig.java
@@ -3,6 +3,8 @@ package security.springsecurityinactionch2ex1.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @Configuration
 public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
